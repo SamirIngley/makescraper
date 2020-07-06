c70575a891b5d52ef196f1a24a220d53a575c75b
SamirIngley <samir.ingle7@gmail.com>
2020-07-06 13:02:00 -0700

    hackernews, title and link pulled, turned to json, can't push to file tho - turns out empty

--- scrape.go
+++ scrape.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/gocolly/colly"
 )
 
-type post struct{ Data string }
+type post struct {
+	title string `json:"title"`
+	link  string `json:"link"`
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
+	var posts []post
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
+	// span.a-size-medium sc-product-title a-text-bold
+	c.OnHTML(".title a", func(e *colly.HTMLElement) {
+
+		// fmt.Printf(e.Attr("a"))
+
+		indivTitle := e.Text
+		indivLink := e.Attr("href")
+		posts = append(posts, post{title: indivTitle, link: indivLink})
 
 		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Printf("%q \n%s\n \n", indivTitle, indivLink)
+
 	})
 
 	// Before making a request print "Visiting ..."
@@ -27,6 +40,12 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	// Start scraping 1. cart, 2. all carts (fresh, whole foods, amazon)
+	// c.Visit("https://www.amazon.com/gp/cart/view.html?ref_=nav_cart")
+	c.Visit("https://news.ycombinator.com/")
+	// fmt.Printf("%+v\n", posts)
+
+	// Write json to file
+	postsJSON, _ := json.Marshal(posts)
+	ioutil.WriteFile("postsFile.json", postsJSON, 0644)
 }
